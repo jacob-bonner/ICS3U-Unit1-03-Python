820cb408fc9173501dd7ba3d8010f0f42750203f
Ubuntu <ubuntu@ip-172-31-27-73.ec2.internal>
2019-09-12 18:42:58 +0000

    python hello world

---
+++ hello_world.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+# Created by: Jacob Bonner
+# Created on: September 2019
+# This is a "Hello, World!" program in proper format
+
+
+def main():
+    print("Hello, World!")
+
+
+if __name__ == "__main__":
+    main()
